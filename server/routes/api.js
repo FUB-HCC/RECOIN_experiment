b7ebeadee2e36d87c7c253b700327e37e6320b28
Gobie Nanthakumar <gobie@clear-canvas.com>
2018-10-12 13:22:09 +0200

    backend: log request

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/event/', function (req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ "response": "ok" }));
+	res.setHeader('Content-Type', 'application/json');
+	console.log('req', req);
+	
+	res.send(JSON.stringify({
+		"response": "ok"
+	}));
 });
 
 module.exports = router;
