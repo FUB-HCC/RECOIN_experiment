93f31876c5fcceb09780347a8a2c5105e8eeb0b2
Gobie Nanthakumar <gobie@clear-canvas.com>
2018-10-13 14:56:46 +0200

    save post request to mongoDB

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,18 +1,53 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const mongo = require('../services/mongo');
 
 app.post('/event', function (req, res) {
 	let property = req.body.property;
 	let value = req.body.value;
+	let recoin = req.body.recoin;
+	let workerID = 1;
 
 	let response = {
 		success: true,
-		property: property,
-		value: value
+		workerID,
+		property,
+		value,
+		recoin
 	}
+
+	mongo.connectToServer((res) => {
+		console.log('res from server', res);
+
+		if (res.isConnected) {
+			insertBalance({
+				'workerID': workerID,
+				'res': response
+			}, 'balance', (result) => {
+				let id = result.insertedId;
+
+				findBalance({
+					'_id': String(id)
+				}, 'balance', (res) => {
+					console.log(res);
+					mongo.close();
+				});
+			});
+
+		}
+	});
+
 	res.send(response);
 	res.end("hey");
 });
+
+const insertBalance = (data, collection, callback) => {
+	mongo.insertData(data, collection, callback);
+}
+
+const findBalance = (data, collection, callback) => {
+	mongo.findData(data, collection, callback);
+}
 
 module.exports = app;
